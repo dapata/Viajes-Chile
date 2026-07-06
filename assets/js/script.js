72fb5212a46b46647a4899e4097c99e452fd940f
el.diac <el.diac@gmail.com>
2026-07-05 22:38:29 -0400

    Implementacion de la seccion Destacados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,10 @@
 $(function () {
 
-    /*
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-    */
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
+    $('.card-title').click(function () {
+        $('.card-text').toggle();
+    });
 
 });
